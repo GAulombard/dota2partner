3f5b2351d55a4705296608aed26aeb585bd81a9a
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2022-04-30 10:45:32 +0200

    first commit

---
+++ src/main/java/com/hodor/dota2partner/Dota2PartnerApplication.java
@@ -0,0 +1,13 @@
+package com.hodor.dota2partner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dota2PartnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dota2PartnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hodor/dota2partner/Dota2PartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodor.dota2partner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dota2PartnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
