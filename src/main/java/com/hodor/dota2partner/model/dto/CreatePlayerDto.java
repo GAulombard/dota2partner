c8d68de837582e6531f1b89f17d0f3e4e13bc3e9
Hodor Dell <geoffrey.aulombard@gmail.com>
2022-05-08 00:29:40 +0200

    WIP: refactoring with DTO concept
    Signed-off-by: Hodor Dell <geoffrey.aulombard@gmail.com>

--- src/main/java/com/hodor/dota2partner/model/dto/CreatePlayerDto.java
+++ src/main/java/com/hodor/dota2partner/model/dto/CreatePlayerDto.java
@@ -1,20 +1,23 @@
 package com.hodor.dota2partner.model.dto;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.validation.constraints.*;
 import java.io.Serializable;
 
-@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class CreatePlayerDto implements Serializable {
 
     @NotNull(message = "Steam Id is mandatory")
-    private final Long steamId64;
+    private Long steamId64;
     @NotEmpty(message = "Email is mandatory")
     @Email(message = "Email is not valid")
-    private final String email;
+    private String email;
     @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
             message = "Password must contain at least 1 uppercase char, 8 characters, 1 digit, and 1 symbol")
     @NotBlank(message = "Password is mandatory")
-    private final String password;
+    private String password;
 }
--- src/main/java/com/hodor/dota2partner/model/dto/PlayerDto.java
+++ src/main/java/com/hodor/dota2partner/model/dto/PlayerDto.java
@@ -1,15 +1,21 @@
 package com.hodor.dota2partner.model.dto;
 
-import lombok.Data;
+import lombok.*;
 
 import java.io.Serializable;
 
-@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class PlayerDto implements Serializable {
-    private final Long steamId32;
-    private final String personaName;
-    private final int rankTier;
-    private final int win;
-    private final int loss;
-    private final float winRate;
+
+    private Long steamId32;
+    private String personaName;
+    private int rankTier;
+    private int win;
+    private int loss;
+    private float winRate;
+
 }
