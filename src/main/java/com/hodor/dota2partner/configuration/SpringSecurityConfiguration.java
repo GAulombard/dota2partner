c196746feffd4f671cb491735512b420ad3a2be5
Hodor Dell <geoffrey.aulombard@gmail.com>
2022-05-30 22:49:39 +0200

    WIP: working on the front-end
    Signed-off-by: Hodor Dell <geoffrey.aulombard@gmail.com>

--- src/main/java/com/hodor/dota2partner/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/hodor/dota2partner/configuration/SpringSecurityConfiguration.java
@@ -24,6 +24,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
                                     "/",
                                     "/css/**",
                                     "/player/validate",
+                                    "/assets/*",
                                     "/api/**",
                                     "/login",
                                     "/signup",
